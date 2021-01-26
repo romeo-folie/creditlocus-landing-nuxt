9ef7fe0cdf1dcabca67146b1093686fe78e1dd0c
Romeo Folie <romeofolie1@gmail.com>
2021-01-26 22:05:31 +0000

    added transition to side menu

--- components/Aside.vue
+++ components/Aside.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside id="side-nav" class="side-menu">
+  <aside id="side-nav" class="side-menu slide-in-right">
     <div class="aside-content-wrapper">
       <div class="btn-container">
         <a href="javascript:void(0);" onclick="hideSideNav()">
@@ -39,4 +39,35 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.slide-in-right {
+  -webkit-animation: slide-in-right 1s cubic-bezier(0.785, 0.135, 0.15, 0.86)
+    0.5s both;
+  animation: slide-in-right 1s cubic-bezier(0.785, 0.135, 0.15, 0.86) 0.5s both;
+}
+
+@-webkit-keyframes slide-in-right {
+  0% {
+    -webkit-transform: translateX(1000px);
+    transform: translateX(1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-right {
+  0% {
+    -webkit-transform: translateX(1000px);
+    transform: translateX(1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+</style>
