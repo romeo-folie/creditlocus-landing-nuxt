ab2011b81a3dc96e5e2ca907b3734b8024c6efc3
Romeo Folie <romeofolie1@gmail.com>
2021-01-27 15:41:43 +0000

    updated cash flow section css

--- components/Overlay.vue
+++ components/Overlay.vue
@@ -2,8 +2,21 @@
   <div id="overlay" class="overlay">
     <div class="close-btn" onclick="hideVideo()">
       <a href="javascript:void();"
-        ><img src="/icons/x.svg" alt="close icon"
-      /></a>
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#fff"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-x"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line></svg
+      ></a>
     </div>
   </div>
 </template>
