68adff33e26b883cf0f9f5af8b3dad00f70b6ffd
Romeo Folie <romeofolie1@gmail.com>
2021-03-14 10:58:43 +0000

    updated about info

--- components/About/CompanyInfoSection.vue
+++ components/About/CompanyInfoSection.vue
@@ -1,16 +1,17 @@
 <template>
   <section class="company-info">
     <div class="company-info-content">
-      <h3>Company Information</h3>
+      <!-- <h3>Company Information</h3> -->
 
-      <h5>Office Address</h5>
+      <h5>Email Address</h5>
 
       <p id="address">
-        57554 kuhlman Keys Suite 435
+        <!-- 57554 kuhlman Keys Suite 435
         <br />
         North Hoseaside, SD 17258-9946
         <br />
-        reachout@creditlocus.com
+        reachout@creditlocus.com -->
+        admin@creditlocus.com
       </p>
     </div>
   </section>
